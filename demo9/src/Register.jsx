6d048d3257eda01959da369aabd9dca216f9dd87
AnjanaBiju152 <anjana.biju200@gmail.com>
2025-02-22 13:01:47 +0530

    react notes

--- demo9/src/Register.jsx
+++ demo9/src/Register.jsx
@@ -10,40 +10,51 @@ function Register() {
     })
     const registerUser = (e) => {
         e.preventDefault()
-    
+        console.log("final data");
+        console.log(userData);
+
 
     }
-    const getUserData=(e)=> {
-        console.log(e.target.value);
-        console.log(e.target.name);
-        
-        
+    const getUserData = (e) => {
+        let fieldName = e.target.name;
+        let userValue = e.target.value;
+
+        setUserData({ ...userData, [fieldName]: userValue })
+
+
+
     }
     return (
         <>
             <h2 style={{ color: 'green' }}>Register form</h2>
             <form onSubmit={registerUser}>
+
+
                 <label htmlFor="">First Name</label>
-                <input type="text" placeholder="Enter name" name="uName" onChange={(e)=>getUserData(e)}
+                <input type="text" placeholder="Enter name" name="uName" onChange={(e) => getUserData(e)}
                     style={{ marginLeft: '10px', borderRadius: '5px', padding: '10px', width: '250px' }} />
+
                 <div style={{ marginTop: '20px' }}>
+
                     <label htmlFor="" style={{ fontSize: '20px' }}>Gender</label>
-                    <input type="radio" name="gender" id="" style={{ marginLeft: '20px' }} />
+                    <input type="radio" name="uGender" id="" style={{ marginLeft: '20px' }} value="male" onChange={(e) => getUserData(e)} />
                     <label htmlFor="">Male</label>
-                    <input type="radio" name="gender" id="" />
+
+                    <input type="radio" name="uGender" id="" value="female" onChange={(e) => getUserData(e)} />
                     <label htmlFor="">Female</label>
-                    <input type="radio" name="gender" id="" />
+
+                    <input type="radio" name="uGender" id="" value="others" onChange={(e) => getUserData(e)} />
                     <label htmlFor="">Other</label>
 
                 </div>
                 <div style={{ marginTop: '20px' }}>
 
                     <label htmlFor="">COURSE</label>
-                    <select name="" id="" style={{ marginLeft: '20px' }} >
-                        <option value="">CSE</option>
-                        <option value="">CIVIL</option>
-                        <option value="">MEC</option>
-                        <option value="">ECE</option>
+                    <select name="uCourse" id="" style={{ marginLeft: '20px' }} onChange={(e) => getUserData(e)}>
+                        <option value="Cse">CSE</option>
+                        <option value="Civil">CIVIL</option>
+                        <option value="Mec">MEC</option>
+                        <option value="Ece">ECE</option>
                     </select>
                 </div>
                 <div>
